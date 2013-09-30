b8a3ebe2a15cfdf365397a460498a95e93ec0f1a
Xiaoming Wang <xiaoming.wang@lexisnexis.com>
2013-09-30 16:21:31 -0400

    HPCC-9723 change Package name to allow multiple clienttools installed
    Change ClientTools package name to "hpccsystems-clienttools-<major version>.<mintor version>
    This will allow multiple clienttools with different <major version>.<mintor version> installed
    on the systems. For example following clienttools can co-exist on a system:
       hpccsystems-clienttools-4.0_community-4.0.2-2el6_x86_64.rpm
       hpccsystems-clienttools-4.2_community-4.2.0-1el6_x86_64.rpm
    
    On the other hand if <major version>.<mintor version> are the same the newer package can be updated
    on the top of older package. for example,
    1) With different pack number:
       hpccsystems-clienttools-4.2_community-4.2.2-1el6_x86_64.rpm can be updated on top of
       hpccsystems-clienttools-4.2_community-4.2.0-1el6_x86_64.rpm
    
    2) With different build(sequence) number
       hpccsystems-clienttools-4.2_community-4.2.0-2el6_x86_64.rpm can be updated on top of
       hpccsystems-clienttools-4.2_community-4.2.0-1el6_x86_64.rpm

--- version.cmake
+++ version.cmake
@@ -4,7 +4,7 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 4 )
 set ( HPCC_MINOR 1 )
-set ( HPCC_POINT 0 )
+set ( HPCC_POINT 3 )
 set ( HPCC_MATURITY "trunk" )
-set ( HPCC_SEQUENCE 1 )
+set ( HPCC_SEQUENCE 5 )
 ###
